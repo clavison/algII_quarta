de4b50121cb0ad414068084f8ad633376f6986b9
clavison <clavison@gmail.com>
2024-03-20 20:21:53 -0300

    Correção exercício 1

---
+++ src/vetores/Exercicio1.java
@@ -0,0 +1,17 @@
+package vetores;
+
+import javax.swing.JOptionPane;
+
+public class Exercicio1 {
+	
+	public static void main(String[] args) {
+		String[] cores = new String[5];
+		for(int i=0; i<cores.length; i++) {
+			cores[i] = JOptionPane.showInputDialog("Cor:");
+		}
+		for(int i=cores.length-1; i>=0; i--) {
+			System.out.println(cores[i]);
+		}
+	}
+
+}
